a3c0e70643b938f9dd6f7617225ea1488b15f770
Naveen Thurimerla <nawinto99@gmail.com>
2021-10-25 09:16:25 +0530

    :fire: fix the format

--- src/components/CTA.js
+++ src/components/CTA.js
@@ -1,6 +1,6 @@
-import { Link as ChakraLink, Button } from '@chakra-ui/react'
+import { Link as ChakraLink, Button } from "@chakra-ui/react";
 
-import { Container } from './Container'
+import { Container } from "./Container";
 
 export const CTA = () => (
   <Container
@@ -10,7 +10,5 @@ export const CTA = () => (
     width="100%"
     maxWidth="48rem"
     py={2}
-  >
-    
-  </Container>
-)
+  ></Container>
+);
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,27 +5,24 @@ import {
   List,
   ListIcon,
   ListItem,
-} from '@chakra-ui/react'
-import { CheckCircleIcon, LinkIcon } from '@chakra-ui/icons'
-import { Hero } from '../components/Hero'
-import { Container } from '../components/Container'
-import { Main } from '../components/Main'
-import { DarkModeSwitch } from '../components/DarkModeSwitch'
-import { CTA } from '../components/CTA'
-import { Footer } from '../components/Footer'
+} from "@chakra-ui/react";
+import { CheckCircleIcon, LinkIcon } from "@chakra-ui/icons";
+import { Hero } from "../components/Hero";
+import { Container } from "../components/Container";
+import { Main } from "../components/Main";
+import { DarkModeSwitch } from "../components/DarkModeSwitch";
+import { CTA } from "../components/CTA";
+import { Footer } from "../components/Footer";
 
 const Index = () => (
   <Container height="100vh">
     <Hero />
-    <Main>
-      
-    </Main>
+    <Main></Main>
 
     <DarkModeSwitch />
-    <Footer>
-    </Footer>
+    <Footer></Footer>
     <CTA />
   </Container>
-)
+);
 
-export default Index
+export default Index;
