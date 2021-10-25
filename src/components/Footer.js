eb408d524ebbfe9db5d8486063880c2a04c9d226
Naveen Thurimerla <nawinto99@gmail.com>
2021-10-25 09:09:02 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,29 +12,26 @@ export const Footer = (props) => (
     py="12"
     px={{ base: "4", md: "8" }}
   >
-    <Stack
-      direction={{ base: "column-reverse", md: "row" }}
-      justifyContent="space-between"
-      alignItems="center"
-    >
-      <Text fontSize="sm" {...props}>
+    <Stack>
+      <Stack direction="row" spacing="4" align="center" justify="space-between">
+        <ButtonGroup variant="ghost" color="gray.600" {...props}>
+          <IconButton
+            as="a"
+            href="https://github.com/datadlog"
+            aria-label="GitHub"
+            icon={<FaGithub fontSize="20px" />}
+          />
+          <IconButton
+            as="a"
+            href="https://twitter.com/datadlog"
+            aria-label="Twitter"
+            icon={<FaTwitter fontSize="20px" />}
+          />
+        </ButtonGroup>
+      </Stack>
+      <Text fontSize="sm" alignSelf={{ base: "center", sm: "start" }}>
         &copy; {new Date().getFullYear()} DataDlog. All rights reserved.
       </Text>
-
-      <ButtonGroup variant="ghost" color="gray.600" {...props}>
-        <IconButton
-          as="a"
-          href="https://github.com/datadlog"
-          aria-label="GitHub"
-          icon={<FaGithub fontSize="20px" />}
-        />
-        <IconButton
-          as="a"
-          href="https://twitter.com/datadlog"
-          aria-label="Twitter"
-          icon={<FaTwitter fontSize="20px" />}
-        />
-      </ButtonGroup>
     </Stack>
   </Box>
 );
