1a2607a0ca60cc30335e114b01b7d044b90e1ce7
Naveen Thurimerla <nawinto99@gmail.com>
2021-10-25 09:01:33 +0530

    change footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,40 @@
-import { Flex } from '@chakra-ui/react'
+import { Box, Stack, ButtonGroup, IconButton } from "@chakra-ui/react";
+import { Text } from "@chakra-ui/layout";
+import * as React from "react";
+import { FaGithub, FaTwitter } from "react-icons/fa";
 
-export const Footer = (props) => <Flex as="footer" py="8rem" {...props} />
+export const Footer = (props) => (
+  <Box
+    as="footer"
+    role="contentinfo"
+    mx="auto"
+    maxW="7xl"
+    py="12"
+    px={{ base: "4", md: "8" }}
+  >
+    <Stack
+      direction={{ base: "column-reverse", md: "row" }}
+      justifyContent="space-between"
+      alignItems="center"
+    >
+      <Text fontSize="sm" {...props}>
+        &copy; {new Date().getFullYear()} DataDlog. All rights reserved.
+      </Text>
+
+      <ButtonGroup variant="ghost" color="gray.600" {...props}>
+        <IconButton
+          as="a"
+          href="https://github.com/datadlog"
+          aria-label="GitHub"
+          icon={<FaGithub fontSize="20px" />}
+        />
+        <IconButton
+          as="a"
+          href="https://twitter.com/datadlog"
+          aria-label="Twitter"
+          icon={<FaTwitter fontSize="20px" />}
+        />
+      </ButtonGroup>
+    </Stack>
+  </Box>
+);
